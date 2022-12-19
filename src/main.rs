e7311aae5b27063f93d0a8d3add96ec20e1dabaa
Abdullah Ahmad <abdullah.ahmad@strath.ac.uk>
2022-12-19 17:56:52 +0000

    Trying to make axis_iter() work

--- src/main.rs
+++ src/main.rs
@@ -22,17 +22,19 @@ fn main() {
     println!("{:?}", test + other_test);
     println!("\nNDARRAY EXPT");
     let input_vec: Vec<f64> = linspace::<f64>(0., 20.48, 16*3*3).collect();
-    println!("{:?}", input_vec);
-    let mut input_array = Array3::<f64>::zeros((16, 3, 3));
+    let input_array = Array3::<f64>::zeros((16, 3, 3));
     let test_ndarray = Array::from_shape_vec((16, 3, 3), input_vec).unwrap();
-    let ndarray2 = test_ndarray.clone();
-    let ndarray3 = test_ndarray.clone();
+    let test_ndarray2 = test_ndarray.clone();
+    let test_ndarray3 = test_ndarray.clone();
     ndarray::Zip::from(input_array.axis_iter_mut(Axis(0)))
         .and(test_ndarray.axis_iter(Axis(0)))
-        .and(ndarray2.axis_iter(Axis(0)))
-        .and(ndarray3.axis_iter(Axis(0)))
+        .and(test_ndarray2.axis_iter(Axis(0)))
+        .and(test_ndarray3.axis_iter(Axis(0)))
         .for_each(
-            |inp, a, b, c|
-            inp = a.view()
-        )
+            |mut inp, a, b, c|
+            // Want to perform a matrix multiplication
+            // A * B * C where all matrices are NxN matrices
+            // and save the result in inp - preallocated array
+            inp = a
+                )
 }
