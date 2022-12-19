3dd05bc8e53ef288e16fa7b6c70c3a4f828e1565
Abdullah Ahmad <abdullah.ahmad@strath.ac.uk>
2022-12-19 11:42:31 +0000

    Experimenting with ndarray iterators

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,11 @@
 mod custom_grid;
 
 use crate::custom_grid::GridArray;
+use ndarray::prelude::*;
+use itertools_num::linspace;
 
 fn main() {
+    println!("CUSTOM GRIDARRAY EXPT");
     let mut test = GridArray::<f64>::new();
     println!("Initialised GridArray\n{:?}", test);
     test.push(1.01);
@@ -17,4 +20,19 @@ fn main() {
     test[1] = 2.0;
     println!("Adding {:?} and {:?}", test, other_test);
     println!("{:?}", test + other_test);
+    println!("\nNDARRAY EXPT");
+    let input_vec: Vec<f64> = linspace::<f64>(0., 20.48, 16*3*3).collect();
+    println!("{:?}", input_vec);
+    let mut input_array = Array3::<f64>::zeros((16, 3, 3));
+    let test_ndarray = Array::from_shape_vec((16, 3, 3), input_vec).unwrap();
+    let ndarray2 = test_ndarray.clone();
+    let ndarray3 = test_ndarray.clone();
+    ndarray::Zip::from(input_array.axis_iter_mut(Axis(0)))
+        .and(test_ndarray.axis_iter(Axis(0)))
+        .and(ndarray2.axis_iter(Axis(0)))
+        .and(ndarray3.axis_iter(Axis(0)))
+        .for_each(
+            |inp, a, b, c|
+            inp = a.view()
+        )
 }
